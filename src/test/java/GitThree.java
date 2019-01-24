1794a4964ed561210e8146bf654d39d8842afd9d
h1patel <h1patel@icloud.com>
2019-01-24 05:14:54 -0500

    error fixed

--- src/test/java/GitThree.java
+++ src/test/java/GitThree.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.testng.annotations.Test;
 
 public class GitThree {
@@ -9,19 +8,6 @@ public class GitThree {
 		System.out.println("Git Three Test Print");
 		System.out.println("Success try from Bash");
 		System.out.println("Change from PC");
-	}
-}
-=======
-import org.testng.annotations.Test;
-
-public class GitThree {
-
-	@Test
-	private void testprint() {
-
-		System.out.println("Git Three Test Print");
-		System.out.println("Success try from Bash");
 		System.out.println("next try for Change");
 	}
 }
->>>>>>> branch 'master' of https://github.com/30458/GitTestThree
