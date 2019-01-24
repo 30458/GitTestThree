d968a6252238de4a275ac366b2a019f7025a34a0
h1patel <h1patel@icloud.com>
2019-01-24 05:21:04 -0500

    third change

--- src/test/java/GitThree.java
+++ src/test/java/GitThree.java
@@ -7,7 +7,6 @@ public class GitThree {
 
 		System.out.println("Git Three Test Print");
 		System.out.println("Success try from Bash");
-		System.out.println("Change from PC");
 		System.out.println("next try for Change");
 	}
 }
