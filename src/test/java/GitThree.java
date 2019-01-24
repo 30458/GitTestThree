98c11890d48bc243ffd3115661d111034f01106a
h1patel <h1patel@icloud.com>
2019-01-24 04:49:50 -0500

    Test2 - Same change

--- src/test/java/GitThree.java
+++ src/test/java/GitThree.java
@@ -7,7 +7,6 @@ public class GitThree {
 
 		System.out.println("Git Three Test Print");
 		System.out.println("Success try from Bash");
-		System.out.println("Once Again - Hello from Desktop");
 		
 	}
 }
