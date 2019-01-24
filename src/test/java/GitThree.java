89cacfdd564687d4ad494f4c9130200bab2caa9a
test1 <h1patel@icloud.com>
2019-01-24 05:53:37 -0500

    change 333

--- src/test/java/GitThree.java
+++ src/test/java/GitThree.java
@@ -7,11 +7,7 @@ public class GitThree {
 
 		System.out.println("Git Three Test Print");
 		System.out.println("Success try from Bash");
-<<<<<<< HEAD
 		System.out.println("Third Change from PC");
 		System.out.println("next try for Change");
-		
-=======
->>>>>>> dc08835e75923b8ab1475b3ee4ada1759d044988
 	}
 }
